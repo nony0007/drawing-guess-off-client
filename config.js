5441d18207e980570c856c61f966166aaa5c4ccf
nony0007 <90529894+nony0007@users.noreply.github.com>
2025-08-09 19:34:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
 // Replace with your deployed server URL (Render/Heroku/other).
-// Example: const SERVER_URL = "https://drawing-guess-off.onrender.com";
-https://drawing-guess-off-server.onrender.com
+const SERVER_URL = "https://drawing-guess-off-server.onrender.com";
